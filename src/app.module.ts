bb9b3a0a294902a99e9f00bdfaf8716bc613c43e
Pham Duy <duy.ph@briswell-vn.com>
2022-12-20 10:23:38 +0700

    Add Interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleModule } from './modules/role/role.module';
 import { UserModule } from './modules/user/user.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ErrorsInterceptor } from './interceptors/errors.interceptor';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 @Module({
   imports: [
@@ -12,6 +15,16 @@ import { UserModule } from './modules/user/user.module';
     TypeOrmModule.forRoot(),
     UserModule,
     RoleModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorsInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
+    },
   ],
 })
 export class AppModule {}
